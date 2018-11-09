f57d81300ff073115800bd7e4f56577c43ae2c62
Zihao Zheng <zzh523710043@tamu.edu>
2018-11-09 02:32:23 -0600

    Finger Movement via mouse input complete(need to add limitation)
    Also add 2nd camera as `display2`

---
+++ Assets/root_finger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//tribution to tutorial and code used
+//http://aarlangdi.blogspot.com/2016/04/unity-tutorial-rotate-object-with-mouse.html
+//https://aarlangdi.blogspot.com/2016/07/rotate-object-with-another-object-in.html
+//https://www.youtube.com/watch?v=Z9O0u0EmtiI&feature=youtu.be
+
+public class root_finger : MonoBehaviour {
+
+    /*float rotSpeed = 20;
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+        transform.RotateAround(Vector3.up, -rotX);
+        transform.RotateAround(Vector3.right, rotY);
+        //transform.Rotate(Vector3.down, rotY);
+    }
+    */
+    public Vector3 Pivot = Vector3.down;
+   
+    //it could be a Vector3 but its more user friendly
+    public bool RotateX = false;
+    public bool RotateY = false;
+    public bool RotateZ = true;
+
+    float rotSpeed = 20;
+
+    void OnMouseDrag()
+    {
+        transform.position += (transform.rotation*Pivot);
+       
+        if (RotateX)
+            transform.rotation *= Quaternion.AngleAxis(rotSpeed * Input.GetAxis("Mouse X"), Vector3.right);
+        if (RotateY)
+            transform.rotation *= Quaternion.AngleAxis(rotSpeed * Input.GetAxis("Mouse Y"), Vector3.up);
+        if (RotateZ)
+            transform.rotation *= Quaternion.AngleAxis(-1 * rotSpeed * Input.GetAxis("Mouse Y"), Vector3.forward);
+ 
+        transform.position -= (transform.rotation*Pivot);
+
+    }
+    
+}
---
+++ Assets/snd_finger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snd_finger : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject obj;
+
+    void Update()
+    {
+        transform.rotation = obj.transform.rotation;
+    }
+}
